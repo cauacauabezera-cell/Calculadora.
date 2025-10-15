f9cc44ad75dcc25f829072e02ccf02744a532a41
CC <cauacauabezera@gmail.com>
2025-10-15 16:47:40 -0300

    Initial commit

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+let x = 10, y = 2;
+
+// Adição
+let soma = x + y;
+console.log(`A soma de ${x} e ${y} é ${soma}`);
+
+// Subtração
+let subtracao = x - y;
+console.log(`A subtração de ${x} e ${y} é ${subtracao}`);
+
+// Multiplicação
+let multiplicacao = x * y;
+console.log(`A multiplicação de ${x} e ${y} é ${multiplicacao}`);
+
+// Divisão
+let divisao = x / y;
+console.log(`A divisão de ${x} e ${y} é ${divisao}`);
