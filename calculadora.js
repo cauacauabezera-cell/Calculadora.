efcc866c06afaa68f67e20f1da3a6865e3ddfddb
CC <cauacauabezera@gmail.com>
2025-10-20 16:09:53 -0300

    Terceiro commit

--- calculadora.js
+++ calculadora.js
@@ -1,42 +1,34 @@
-let x = 10;
-let y = 2;
-let operador = '+';
-
-// Verifica x
-if (typeof x !== 'number') {
-  console.log("Erro: x deve ser um número válido.");
-} 
-else if (isNaN(x)) {
-  console.log("Erro: x deve ser um número válido.");
-} 
-
-// Verifica y
-else if (typeof y !== 'number') {
-  console.log("Erro: y deve ser um número válido.");
-} 
-else if (isNaN(y)) {
-  console.log("Erro: y deve ser um número válido.");
-} 
-
-// Operações
-else if (operador === '+') {
-  console.log(`A soma de ${x} e ${y} é ${x + y}`);
-} 
-else if (operador === '-') {
-  console.log(`A subtração de ${x} e ${y} é ${x - y}`);
-} 
-else if (operador === '*') {
-  console.log(`A multiplicação de ${x} e ${y} é ${x * y}`);
-} 
-else if (operador === '/') {
-  if (y === 0) {
-    console.log("Erro: divisão por zero não é permitida.");
-  } else {
-    console.log(`A divisão de ${x} e ${y} é ${x / y}`);
+function calcular(numero1, numero2, operador) {
+  // Verifica numero1
+  if (typeof numero1 !== 'number' || isNaN(numero1)) {
+    console.log("Erro: o primeiro número deve ser válido.");
+    return;
   }
-} 
 
-// Caso o operador não seja válido
-else {
-  console.log("Erro: operador inválido. Use +, -, * ou /.");
+  // Verifica numero2
+  if (typeof numero2 !== 'number' || isNaN(numero2)) {
+    console.log("Erro: o segundo número deve ser válido.");
+    return;
+  }
+
+  // Operações
+  if (operador === '+') {
+    console.log(`A soma de ${numero1} e ${numero2} é ${numero1 + numero2}`);
+  } 
+  else if (operador === '-') {
+    console.log(`A subtração de ${numero1} e ${numero2} é ${numero1 - numero2}`);
+  } 
+  else if (operador === '*') {
+    console.log(`A multiplicação de ${numero1} e ${numero2} é ${numero1 * numero2}`);
+  } 
+  else if (operador === '/') {
+    if (numero2 === 0) {
+      console.log("Erro: divisão por zero não é permitida.");
+    } else {
+      console.log(`A divisão de ${numero1} e ${numero2} é ${numero1 / numero2}`);
+    }
+  } 
+  else {
+    console.log("Erro: operador inválido. Use +, -, * ou /.");
+  }
 }
